5d117bc02aa0ecc1b5673dbb3b953385f7d9f50f
felixjoby2003 <121381566@umail.ucc.ie>
2023-11-27 17:07:29 +0000

    Implemented the constructor and getter code to fix testColourTableConstructor().

--- src/main/java/ColourTable.java
+++ src/main/java/ColourTable.java
@@ -1,2 +1,10 @@
 public class ColourTable {
+    int numOfColours;
+    public ColourTable(int numOfColours) {
+        this.numOfColours = numOfColours;
+    }
+
+    public int getNumOfColours() {
+        return numOfColours;
+    }
 }
