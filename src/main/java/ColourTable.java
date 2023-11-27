a7420fd3ff06561624d89c0ecceb92b84ca07a72
felixjoby2003 <121381566@umail.ucc.ie>
2023-11-27 22:16:22 +0000

    Updated and created code to pass the testColourTableParameter test.

--- src/main/java/ColourTable.java
+++ src/main/java/ColourTable.java
@@ -1,10 +1,19 @@
+
 public class ColourTable {
     int numOfColours;
     public ColourTable(int numOfColours) {
+        if (numOfColours <=1 || numOfColours >= 1025 || !isPowerOfTwo(numOfColours)) {
+            throw new IllegalArgumentException();
+        }
         this.numOfColours = numOfColours;
     }
 
     public int getNumOfColours() {
         return numOfColours;
+    }
+
+    public boolean isPowerOfTwo(int num) {
+        return (int)(Math.ceil((Math.log(num) / Math.log(2))))
+                == (int)(Math.floor(((Math.log(num) / Math.log(2)))));
     }
 }
