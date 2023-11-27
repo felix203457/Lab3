274baac022e84f6346b28381e371665bc010162a
felixjoby2003 <121381566@umail.ucc.ie>
2023-11-27 17:00:07 +0000

    Created a test for the constructor in ColourTable class which will obviously fail.

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -1,6 +1,14 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ColourTest {
+    @Test
+    public void testColourTableConstructor() {
+        int numOfColours = 2;
+        ColourTable ct =  new ColourTable(numOfColours);
+        assertNotNull(ct);
+        assertEquals(numOfColours, ct.getNumOfColours());
+    }
 
 }
