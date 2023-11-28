609b81fbef9dfec4ae5665f8221317c12255824f
felixjoby2003 <121381566@umail.ucc.ie>
2023-11-28 01:34:24 +0000

    Implemented addColour() and packRGB() methods so that testAddMethod() runs successfully.

--- src/main/java/ColourTable.java
+++ src/main/java/ColourTable.java
@@ -1,11 +1,14 @@
-
+import java.util.HashSet;
+import java.util.Set;
 public class ColourTable {
+    Set<Integer> colourSet;
     int numOfColours;
     public ColourTable(int numOfColours) {
+        this.numOfColours = numOfColours;
         if (numOfColours <=1 || numOfColours >= 1025 || !isPowerOfTwo(numOfColours)) {
             throw new IllegalArgumentException();
         }
-        this.numOfColours = numOfColours;
+        this.colourSet = new HashSet<>();
     }
 
     public int getNumOfColours() {
@@ -14,5 +17,12 @@ public class ColourTable {
 
     public boolean isPowerOfTwo(int num) {
         return num !=0 && ((num & (num - 1)) == 0);
+    }
+    public void addColour(int red, int green, int blue) {
+        int rgbValue = packRGB(red, green, blue);
+        colourSet.add(rgbValue);
+    }
+    public int packRGB(int red, int green, int blue) {
+        return (red << 16) | (green << 8) | blue;
     }
 }
